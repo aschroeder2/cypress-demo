221a40d5c38026e8c624b9bb914a235431052376
Ashley Schroeder <ashley.schroeder2016@gmail.com>
2021-07-11 16:08:27 -0500

    Header navigation tests
    * Implement test of sub nav dropdown
    
    * Implement main nav item test
    
    * Address pull request comment

--- cypress/integration/semanticbits/header_navigation.spec.ts
+++ cypress/integration/semanticbits/header_navigation.spec.ts
@@ -1,7 +1,27 @@
+/// <reference types="cypress"/>
+
+import { prod } from '../../fixtures/endpoints.json'
+import { HeaderPage } from '../../support/pages/header.page'
+import { BasePage } from '../../support/pages/base.page'
+
 describe('Header navigation', () => {
-  it('User can navigate to the software development page and see available services', () => {
-    cy.visit('https://semanticbits.com/');
-    cy.get('#menu-item-6987').trigger('mouseover');
-    cy.get('#menu-item-6987').should('have.class', 'sfHover')
-  });
-});
+
+  const header = new HeaderPage()
+  const basePage = new BasePage()
+
+  beforeEach(() => {
+    cy.visit(prod.semanticbits_home)
+  })
+
+  it('User can navigate to the Contact page for SemanticBits contact information', () => {
+    header.clickMainNavItem('Contact')
+    basePage.getTopHeaderElem().should('contain.text', 'Contact Us')
+  })
+
+  it('User can navigate to the Software Development page to see available services', () => {
+    header.hoverHeaderMainNavItem('Expertise')
+    header.mainNavElem('Expertise').should('have.class', 'sfHover')
+    header.clickSubMenuItem('Software Development')
+    basePage.getTopHeaderElem().should('contain.text', 'Software Development')
+  })
+})
---
+++ cypress/support/pages/header.page.ts
@@ -0,0 +1,18 @@
+export class HeaderPage {
+
+  hoverHeaderMainNavItem(mainNavItem: string): void {
+    this.mainNavElem(mainNavItem).trigger('mouseover')
+  }
+  
+  clickSubMenuItem(subMenuItem: string): void {
+    cy.contains('ul.sub-menu li a', subMenuItem).click()
+  }
+  
+  clickMainNavItem(mainNavItem: string): void {
+    this.mainNavElem(mainNavItem).click()
+  }
+
+  mainNavElem(mainNavItem: string): Cypress.Chainable<JQuery<HTMLElement>> {
+    return cy.contains('ul.sf-menu li', mainNavItem)
+  }
+}
